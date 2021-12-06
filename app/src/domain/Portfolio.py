a8e791fb2020a1b2c856262f5a731b8862a6ff23
ischo0812 <leesoo.cho@utdallas.edu>
2021-12-06 14:56:15 -0600

    Main clean

--- app/src/domain/Portfolio.py
+++
@@ -1,9 +0,0 @@
-
-# create a class that mimics the database table: portfolio
-
-class Portfolio():
-    def __init__(self, quantity, ticker, purchase_price, account_number: int = -1):
-        self.quantity = quantity
-        self.ticker = ticker
-        self.purchase_price = purchase_price
-        self.account_number = account_number
